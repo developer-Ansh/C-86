02c96cc2e051d0efe371d8df89cfc1408bfdc0a0
developer-Ansh <74807349+developer-Ansh@users.noreply.github.com>
2023-04-27 11:45:24 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD95FMZLZB_hzu3iUMQ4HYSoORgHkUPqWw",
+  authDomain: "class-85-aa224.firebaseapp.com",
+  projectId: "class-85-aa224",
+  storageBucket: "class-85-aa224.appspot.com",
+  messagingSenderId: "9748931299",
+  appId: "1:9748931299:web:c5a7a75d647378e46d5bd6"
+};
+
